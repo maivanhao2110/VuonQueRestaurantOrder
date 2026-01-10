68e1a92409017dfdf7dbbe829a610cb1cb31fb6b
maivanhao2110 <maivanhao2110@gmail.com>
2026-01-10 11:47:36 +0700

    Update database schema

--- database/db_vuonquerestaurant.sql
+++ database/db_vuonquerestaurant.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th1 09, 2026 lúc 11:34 AM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.0.30
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2026 at 05:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `db_vuonquerestaurant`
+-- Database: `db_vuonquerestaurant`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -35,7 +35,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `name`, `description`, `is_active`) VALUES
@@ -48,7 +48,7 @@ INSERT INTO `category` (`id`, `name`, `description`, `is_active`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `invoice`
+-- Table structure for table `invoice`
 --
 
 CREATE TABLE `invoice` (
@@ -61,7 +61,7 @@ CREATE TABLE `invoice` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `menu_item`
+-- Table structure for table `menu_item`
 --
 
 CREATE TABLE `menu_item` (
@@ -76,7 +76,7 @@ CREATE TABLE `menu_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `menu_item`
+-- Dumping data for table `menu_item`
 --
 
 INSERT INTO `menu_item` (`id`, `category_id`, `name`, `price`, `image_url`, `description`, `is_available`, `created_at`) VALUES
@@ -92,7 +92,7 @@ INSERT INTO `menu_item` (`id`, `category_id`, `name`, `price`, `image_url`, `des
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `orders`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
@@ -100,50 +100,24 @@ CREATE TABLE `orders` (
   `customer_name` varchar(100) NOT NULL,
   `table_number` int(11) NOT NULL,
   `staff_id` int(11) DEFAULT NULL,
-  `status` enum('CREATED','CONFIRMED','COOKING','DONE','CANCELLED') DEFAULT 'CREATED',
+  `status` enum('CREATED','CONFIRMED','COOKING','DONE','PAID','CANCELLED') DEFAULT 'CREATED',
   `note` text DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
   `end_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `orders`
+-- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`id`, `customer_name`, `table_number`, `staff_id`, `status`, `note`, `created_at`, `end_at`) VALUES
-(1, 'Khách', 1, NULL, 'DONE', '', '2026-01-04 09:28:47', NULL),
-(2, 'Khách', 1, NULL, 'DONE', '', '2026-01-04 09:31:46', NULL),
-(3, 'Khách', 1, NULL, 'DONE', '', '2026-01-04 09:34:27', NULL),
-(4, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 09:40:30', NULL),
-(5, 'Khách', 1, NULL, 'CANCELLED', '', '2026-01-04 09:42:18', NULL),
-(6, 'Khách', 1, NULL, 'CONFIRMED', '', '2026-01-04 09:45:04', NULL),
-(7, 'Khách', 1, NULL, 'COOKING', '', '2026-01-04 09:59:25', NULL),
-(8, 'Khách', 6, NULL, 'CREATED', '', '2026-01-04 10:12:21', NULL),
-(9, 'Khách', 2, NULL, 'CREATED', '', '2026-01-04 10:15:57', NULL),
-(10, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:17:29', NULL),
-(11, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:20:02', NULL),
-(12, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:26:16', NULL),
-(13, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:32:24', NULL),
-(14, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:32:34', NULL),
-(15, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:34:02', NULL),
-(16, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:46:02', NULL),
-(17, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 10:57:30', NULL),
-(18, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 11:00:13', NULL),
-(19, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 11:11:19', NULL),
-(20, 'Khách', 1, NULL, 'CREATED', 'Không cay', '2026-01-04 12:31:28', NULL),
-(21, 'Khách', 1, NULL, 'CREATED', '', '2026-01-04 12:40:04', NULL),
-(22, 'Khách', 1, NULL, 'CREATED', '', '2026-01-09 15:58:07', NULL),
-(23, 'Khách', 1, NULL, 'DONE', '', '2026-01-09 16:53:20', '2026-01-09 17:32:05'),
-(24, 'Khách', 1, NULL, 'CREATED', '', '2026-01-09 17:21:09', '2026-01-09 17:21:31'),
-(25, 'Khách', 3, NULL, 'CREATED', '', '2026-01-09 17:26:27', '2026-01-09 17:26:36'),
-(26, 'Khách', 7, NULL, 'CREATED', '', '2026-01-09 17:28:11', NULL),
-(27, 'Khách', 3, NULL, 'CREATED', '', '2026-01-09 17:28:20', '2026-01-09 17:29:44'),
-(28, 'Khách', 5, NULL, 'DONE', '', '2026-01-09 17:28:28', '2026-01-09 17:32:21');
+(24, 'Khách', 1, NULL, 'PAID', '', '2026-01-10 11:34:49', '2026-01-10 11:35:51'),
+(25, 'Khách', 1, NULL, 'CREATED', '', '2026-01-10 11:36:18', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `order_item`
+-- Table structure for table `order_item`
 --
 
 CREATE TABLE `order_item` (
@@ -157,58 +131,20 @@ CREATE TABLE `order_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `order_item`
+-- Dumping data for table `order_item`
 --
 
 INSERT INTO `order_item` (`id`, `order_id`, `menu_item_id`, `quantity`, `price`, `status`, `created_at`) VALUES
-(1, 1, 8, 1, 20000.00, 'DONE', '2026-01-04 09:28:47'),
-(2, 2, 3, 1, 50000.00, 'WAITING', '2026-01-04 09:31:46'),
-(3, 3, 8, 1, 20000.00, 'WAITING', '2026-01-04 09:34:27'),
-(4, 4, 4, 1, 35000.00, 'WAITING', '2026-01-04 09:40:30'),
-(5, 5, 7, 1, 15000.00, 'WAITING', '2026-01-04 09:42:18'),
-(6, 6, 8, 1, 20000.00, 'WAITING', '2026-01-04 09:45:04'),
-(7, 7, 8, 1, 20000.00, 'WAITING', '2026-01-04 09:59:25'),
-(8, 7, 7, 1, 15000.00, 'WAITING', '2026-01-04 09:59:25'),
-(9, 8, 8, 1, 20000.00, 'WAITING', '2026-01-04 10:12:21'),
-(10, 9, 8, 15, 20000.00, 'WAITING', '2026-01-04 10:15:57'),
-(11, 9, 7, 14, 15000.00, 'WAITING', '2026-01-04 10:15:57'),
-(12, 9, 6, 1, 5000.00, 'WAITING', '2026-01-04 10:15:57'),
-(13, 9, 1, 1, 48000.00, 'WAITING', '2026-01-04 10:15:57'),
-(14, 9, 4, 1, 35000.00, 'WAITING', '2026-01-04 10:15:57'),
-(15, 9, 2, 1, 45000.00, 'WAITING', '2026-01-04 10:15:57'),
-(16, 9, 3, 1, 50000.00, 'WAITING', '2026-01-04 10:15:57'),
-(17, 9, 5, 1, 30000.00, 'WAITING', '2026-01-04 10:15:57'),
-(18, 10, 8, 1, 20000.00, 'WAITING', '2026-01-04 10:17:29'),
-(19, 10, 7, 1, 15000.00, 'WAITING', '2026-01-04 10:17:29'),
-(20, 11, 7, 1, 15000.00, 'WAITING', '2026-01-04 10:20:02'),
-(21, 11, 6, 1, 5000.00, 'WAITING', '2026-01-04 10:20:02'),
-(22, 12, 8, 1, 20000.00, 'WAITING', '2026-01-04 10:26:16'),
-(23, 12, 7, 3, 15000.00, 'WAITING', '2026-01-04 10:26:16'),
-(24, 12, 2, 1, 45000.00, 'WAITING', '2026-01-04 10:26:16'),
-(25, 13, 8, 1, 20000.00, 'WAITING', '2026-01-04 10:32:24'),
-(26, 14, 2, 1, 45000.00, 'WAITING', '2026-01-04 10:32:34'),
-(27, 15, 6, 1, 5000.00, 'WAITING', '2026-01-04 10:34:02'),
-(28, 16, 2, 1, 45000.00, 'WAITING', '2026-01-04 10:46:02'),
-(29, 17, 8, 1, 20000.00, 'WAITING', '2026-01-04 10:57:30'),
-(30, 18, 8, 2, 20000.00, 'WAITING', '2026-01-04 11:00:13'),
-(31, 19, 7, 17, 15000.00, 'WAITING', '2026-01-04 11:11:19'),
-(32, 20, 2, 5, 45000.00, 'WAITING', '2026-01-04 12:31:28'),
-(33, 21, 1, 4, 48000.00, 'WAITING', '2026-01-04 12:40:04'),
-(34, 21, 4, 1, 35000.00, 'WAITING', '2026-01-04 12:40:04'),
-(35, 21, 2, 4, 45000.00, 'WAITING', '2026-01-04 12:40:04'),
-(36, 22, 8, 1, 20000.00, 'WAITING', '2026-01-09 15:58:07'),
-(37, 23, 8, 1, 20000.00, 'WAITING', '2026-01-09 16:53:20'),
-(38, 23, 4, 1, 35000.00, 'WAITING', '2026-01-09 16:53:20'),
-(39, 24, 8, 1, 20000.00, 'WAITING', '2026-01-09 17:21:09'),
-(40, 25, 8, 1, 20000.00, 'WAITING', '2026-01-09 17:26:27'),
-(41, 26, 7, 1, 15000.00, 'WAITING', '2026-01-09 17:28:11'),
-(42, 27, 2, 1, 45000.00, 'WAITING', '2026-01-09 17:28:20'),
-(43, 28, 5, 1, 30000.00, 'WAITING', '2026-01-09 17:28:28');
+(39, 24, 7, 1, 15000.00, 'WAITING', '2026-01-10 11:34:49'),
+(40, 24, 4, 1, 35000.00, 'WAITING', '2026-01-10 11:34:49'),
+(41, 24, 3, 1, 50000.00, 'WAITING', '2026-01-10 11:34:49'),
+(42, 25, 6, 1, 5000.00, 'WAITING', '2026-01-10 11:36:18'),
+(43, 25, 1, 1, 48000.00, 'WAITING', '2026-01-10 11:36:18');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `order_status_log`
+-- Table structure for table `order_status_log`
 --
 
 CREATE TABLE `order_status_log` (
@@ -223,7 +159,7 @@ CREATE TABLE `order_status_log` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `payment`
+-- Table structure for table `payment`
 --
 
 CREATE TABLE `payment` (
@@ -237,7 +173,7 @@ CREATE TABLE `payment` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `staff`
+-- Table structure for table `staff`
 --
 
 CREATE TABLE `staff` (
@@ -245,6 +181,7 @@ CREATE TABLE `staff` (
   `full_name` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
+  `position` enum('STAFF','MANAGE','ADMIN') NOT NULL,
   `is_active` tinyint(1) DEFAULT 1,
   `created_at` datetime DEFAULT current_timestamp(),
   `cccd` varchar(20) DEFAULT NULL,
@@ -254,38 +191,38 @@ CREATE TABLE `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `invoice`
+-- Indexes for table `invoice`
 --
 ALTER TABLE `invoice`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `order_id` (`order_id`);
 
 --
--- Chỉ mục cho bảng `menu_item`
+-- Indexes for table `menu_item`
 --
 ALTER TABLE `menu_item`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
 
 --
--- Chỉ mục cho bảng `orders`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `staff_id` (`staff_id`);
 
 --
--- Chỉ mục cho bảng `order_item`
+-- Indexes for table `order_item`
 --
 ALTER TABLE `order_item`
   ADD PRIMARY KEY (`id`),
@@ -293,7 +230,7 @@ ALTER TABLE `order_item`
   ADD KEY `menu_item_id` (`menu_item_id`);
 
 --
--- Chỉ mục cho bảng `order_status_log`
+-- Indexes for table `order_status_log`
 --
 ALTER TABLE `order_status_log`
   ADD PRIMARY KEY (`id`),
@@ -301,14 +238,14 @@ ALTER TABLE `order_status_log`
   ADD KEY `changed_by` (`changed_by`);
 
 --
--- Chỉ mục cho bảng `payment`
+-- Indexes for table `payment`
 --
 ALTER TABLE `payment`
   ADD PRIMARY KEY (`id`),
   ADD KEY `invoice_id` (`invoice_id`);
 
 --
--- Chỉ mục cho bảng `staff`
+-- Indexes for table `staff`
 --
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`id`),
@@ -317,95 +254,95 @@ ALTER TABLE `staff`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT cho bảng `invoice`
+-- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `menu_item`
+-- AUTO_INCREMENT for table `menu_item`
 --
 ALTER TABLE `menu_item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT cho bảng `orders`
+-- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT cho bảng `order_item`
+-- AUTO_INCREMENT for table `order_item`
 --
 ALTER TABLE `order_item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
--- AUTO_INCREMENT cho bảng `order_status_log`
+-- AUTO_INCREMENT for table `order_status_log`
 --
 ALTER TABLE `order_status_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `payment`
+-- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `staff`
+-- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `invoice`
+-- Constraints for table `invoice`
 --
 ALTER TABLE `invoice`
   ADD CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
--- Các ràng buộc cho bảng `menu_item`
+-- Constraints for table `menu_item`
 --
 ALTER TABLE `menu_item`
   ADD CONSTRAINT `menu_item_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
--- Các ràng buộc cho bảng `orders`
+-- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
 
 --
--- Các ràng buộc cho bảng `order_item`
+-- Constraints for table `order_item`
 --
 ALTER TABLE `order_item`
   ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   ADD CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`menu_item_id`) REFERENCES `menu_item` (`id`);
 
 --
--- Các ràng buộc cho bảng `order_status_log`
+-- Constraints for table `order_status_log`
 --
 ALTER TABLE `order_status_log`
   ADD CONSTRAINT `order_status_log_ibfk_1` FOREIGN KEY (`order_item_id`) REFERENCES `order_item` (`id`),
   ADD CONSTRAINT `order_status_log_ibfk_2` FOREIGN KEY (`changed_by`) REFERENCES `staff` (`id`);
 
 --
--- Các ràng buộc cho bảng `payment`
+-- Constraints for table `payment`
 --
 ALTER TABLE `payment`
   ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`);
